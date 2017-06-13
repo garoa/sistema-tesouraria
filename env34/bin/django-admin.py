2f4f4cb116cfb75b40c8c599467566f3dbc913e2
vido <vido@usp.br>
2017-06-13 11:25:56 -0300

    Cleanup

--- env34/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/junkemen/Documents/garoa/sistema-tesouraria/env34/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ geral/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GeralConfig(AppConfig):
+    name = 'geral'
