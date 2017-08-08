941414c43f7c52cd12a143b59525bcca0fd92e32
rg3915 <rg3915@yahoo.com.br>
2017-08-07 21:08:29 -0300

    Tentativa de lançamento mensal

--- clock.py
+++ clock.py
@@ -2,10 +2,12 @@
 import os
 import sys
 import django
+from django.utils import timezone
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sistema_tesouraria.settings")
 django.setup()
 
+from django.contrib.auth.models import User
 from geral.models import Lancamento
 
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -13,9 +15,20 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 # Cron roda dia 01, todo mes
+
+
 @sched.scheduled_job('cron', day=1)
 def faz_lancamentos():
-    # TODO
-    print('faz_lancamentos')
+    users = User.objects.all()
+
+    for user in users:
+        Lancamento.objects.create(
+            user=user,
+            autor='s',
+            credito_debito='d',
+            timestamp_comprovante=timezone.now(),
+            valor=85.0,
+            descricao='Lançamento mensalidade mês %s' % timezone.now().month,
+        )
 
 sched.start()
