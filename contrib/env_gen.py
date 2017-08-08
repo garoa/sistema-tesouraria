db664ee8853280fb811aa638eb7ae374dca9c206
vido <vido@usp.br>
2017-08-07 22:17:17 -0300

    PR #2

---
+++ contrib/env_gen.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+'''
+Django SECRET_KEY generator
+'''
+from django.utils.crypto import get_random_string
+
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+
+CONFIG_STRING = """
+export DEBUG=True
+export SECRET_KEY="%s"
+""".strip() % get_random_string(50, chars)
+
+# Writing our configuration file to '.env'
+with open('.env', 'w') as configfile:
+    configfile.write(CONFIG_STRING)
--- sistema_tesouraria/settings.py
+++ sistema_tesouraria/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,10 +22,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "1i4$&we=^%^d)s56y^!^r6amn8fz6m&vr6dv6jd6uf96na+1*f"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Application definition
 
